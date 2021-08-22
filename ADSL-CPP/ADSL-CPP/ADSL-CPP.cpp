99a781c0b1d5674c60c183077861f8f16654b2c0
ianfr <ifriedri@stevens.edu>
2021-08-22 13:49:19 -0400

    Moved all includes to ADSL.h

--- ADSL-CPP/ADSL-CPP/ADSL-CPP.cpp
+++ ADSL-CPP/ADSL-CPP/ADSL-CPP.cpp
@@ -1,12 +1,5 @@
 // STL includes
-#include <iostream>
-#include <cassert>
-// adsl includes
-#include "DataFrame.h"
-#include "Ops.h"
-#include "Util.h"
-#include "STL.h"
-#include "Plot.h"
+#include "ADSL.h"
 
 using namespace std;
 using adsl::DataFrame;
---
+++ ADSL-CPP/ADSL-CPP/ADSL.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+#include <cassert>
+// adsl includes
+#include "DataFrame.h"
+#include "Ops.h"
+#include "Util.h"
+#include "STL.h"
+#include "Plot.h"
