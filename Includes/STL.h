ede8ca51b291f9f50cd7184ff844db291ae206be
Ian Friedrichs <ifriedri@stevens.edu>
2021-11-27 20:12:07 -0500

    Commented out STL.h for now

--- Includes/STL.h
+++ Includes/STL.h
@@ -1,6 +1,9 @@
 #pragma once
 // Lambdas providing wrappers around common STL functions
 
+//NOTE: BECAUSE OF THE FACT THAT DATALISTS USE VECTORS OF UNIONS, IT MAY
+	// BE NECESSARY TO MANUALLY CODE ACCUMULATE, TRANSFORM, ETC
+
 #include <algorithm>
 
 #include "DataFrame.h"
@@ -12,23 +15,23 @@ namespace adsl {
 
 	// Sum a DataList
 	// Double <- DataList
-	auto accumulate = [](DataList& dl) {
-		return std::accumulate(dl.vals.begin(), dl.vals.end(), 0.0);
-	};
-	auto sum = accumulate; // alias
+	// auto dl_accumulate_dbl = [](DataList& dl) {
+	// 	return std::accumulate(dl.vals.begin(), dl.vals.end(), 0.0);
+	// };
+	// auto dl_sum_dbl = dl_accumulate_dbl; // alias
 
 	// Apply a function to every element of a DataList
 	// DataList <- DataList <- double <- double
-	auto transform = [](std::function<double(double)> fun) {
-		auto retFunc = [fun](DataList& dl) {
-			DataList ret;
-			ret.name = dl.name;
-			std::transform(dl.vals.begin(), dl.vals.end(),
-				           std::back_inserter(ret.vals), fun);
-			return ret;
-		};
-		return retFunc;
-	};
+	// auto dl_transform_dbl = [](std::function<double(double)> fun) {
+	// 	auto retFunc = [fun](DataList& dl) {
+	// 		DataList ret;
+	// 		ret.name = dl.name;
+	// 		std::transform(dl.vals.begin(), dl.vals.end(),
+	// 			           std::back_inserter(ret.vals), fun);
+	// 		return ret;
+	// 	};
+	// 	return retFunc;
+	// };
 
 	// Apply a function to every element of a DataList
 	// std::vector<T> <- DataList <- T <- double
