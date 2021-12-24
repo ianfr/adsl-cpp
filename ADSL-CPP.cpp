683276069b32db57c38a36f6abcdb3ca4cad3b5c
Ian Friedrichs <ifriedri@stevens.edu>
2021-12-24 08:25:51 -0500

    ta-lib builds and links successfully

--- ADSL-CPP.cpp
+++ ADSL-CPP.cpp
@@ -13,6 +13,25 @@ int main() {
     DataList col1(&a, DataType::INT,"");
     std::cout << col1.str() << std::endl;
 
+    // test compile.sh & ta-lib
+    TA_Real    closePrice[400];
+    TA_Real    out[400];
+    TA_Integer outBeg;
+    TA_Integer outNbElement;
+
+    for (int i=0; i < 400; i++) {
+        closePrice[i] = (double)i;
+    }
+
+    TA_RetCode retCode = TA_MA( 0, 399,
+        &closePrice[0],
+        30,TA_MAType_SMA,
+        &outBeg, &outNbElement, &out[0] );
+    
+    for( int i=0; i < outNbElement; i++ )
+        printf( "Day %d = %f\n", outBeg+i, out[i] );
+
+
     cout << "Press Enter to exit... ";
     cin.get();
 }
